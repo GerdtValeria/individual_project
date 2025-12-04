bd42ff0f3406489d310a352dae395d09c28a499e
Gerddhdg <Gerddhdg@sddgf.com>
2025-12-04 11:55:51 +0300

    add_code

--- app/services/categories.py
+++ app/services/categories.py
@@ -7,8 +7,8 @@ class CategoryService(BaseService):
     async def get_all_categories(self) -> list[SCategoriesGet]:
         return await CategoriesRepository.get_all()
     
-    async def add_category(self, categories_data: SCategoriesAdd) -> SCategoriesGet:
-        return await CategoriesRepository.add_category(categories_data)
+    async def add_category(self, category_data: SCategoriesAdd) -> SCategoriesGet:
+        return await CategoriesRepository.add_category(category_data)
     
     async def edit_category(self, category_id: int, category_data: SCategoriesAdd) -> None:
         return await CategoriesRepository.edit_category(category_id, category_data)
