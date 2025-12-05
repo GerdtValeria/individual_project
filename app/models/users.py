a32a7a0f4e63ad2fce5c2747a50a0b03cca24440
fhgccsdgv <fhgccsdg@sdfsv.com>
2025-12-05 20:04:26 +0300

    add_code

--- app/models/users.py
+++ app/models/users.py
@@ -10,6 +10,6 @@ class UserModel(Base):
     password = Column(String(50))
 
 role = relationship("RoleModel", back_populates="users")
-bookings = relationship("BookingsModel", back_populates="users")
+bookings = relationship("BookingsModel", back_populates="user")
 rents = relationship("RentsModel", back_populates="users")
 comments = relationship("CommentsModel", back_populates="users")
