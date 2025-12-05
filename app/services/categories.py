d957670c6cd682fdcf3f66a8edb00837b8eb54ff
ddgdhgcv <ddgdhgcv@fhvcdj.com>
2025-12-05 18:23:45 +0300

    ad_code

--- app/services/categories.py
+++ app/services/categories.py
@@ -5,19 +5,19 @@ from app.repositories.categories import CategoriesRepository
 
 
 class CategoryService(BaseService):
-  def __init__(self, db: DBManager | None = None) -> None:
+      def __init__(self, db: DBManager | None = None) -> None:
 
-  async def get_all_categories(self) -> list[SCategoriesGet]:
+      async def get_all_categories(self) -> list[SCategoriesGet]:
         return await self.db.categories.get_all()
     
-  async def get_category(self) -> list[SCategoriesGet]:
+      async def get_category(self) -> list[SCategoriesGet]:
         return await self.db.categories.get_by_name()
 
-  async def add_category(self, category_data: SCategoriesAdd) -> SCategoriesGet:
+      async def add_category(self, category_data: SCategoriesAdd) -> SCategoriesGet:
         return await self.db.categories.add_category(category_data)
     
-  async def edit_category(self, category_id: int, category_data: SCategoriesAdd) -> None:
+      async def edit_category(self, category_id: int, category_data: SCategoriesAdd) -> None:
         return await self.db.categories.edit_category(category_id, category_data)
     
-  async def delete_category(self, category_id: int) -> None:
+      async def delete_category(self, category_id: int) -> None:
         return await self.db.categories.delete_category(category_id)
