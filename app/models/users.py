fe7f8e62ad2e0ebd6ad711e36add90a32d2ed570
fhgccsdgv <fhgccsdg@sdfsv.com>
2025-12-05 20:30:02 +0300

    add_code

--- app/models/users.py
+++ app/models/users.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database.database import Base
-from app.models.bookings import BookingsModel
+
 
 class UserModel(Base):
     __tablename__ = 'users'
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.api.bookings import router as router_bookings
 from app.api.categories import router as router_categories
 from app.api.comments import router as router_comments
 from app.api.images import router as router_images
 from app.api.rents import router as router_rents
 from app.api.roles import router as router_roles
 from app.api.auth import router as router_users
+from app.api.bookings import router as router_bookings
 from app.api.favorites import router as router_favorites
 
 app = FastAPI()
