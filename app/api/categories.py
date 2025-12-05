b0e1f0d6d043a0a44a65a8ad52813098c22b10e2
Gerddhdg <Gerddhdg@sddgf.com>
2025-12-05 11:21:39 +0300

    add_code

--- app/api/categories.py
+++ app/api/categories.py
@@ -1,28 +1,44 @@
-from fastapi import APIRouter
-from app.schemas.categories import SCategoriesAdd
+from fastapi import APIRouter, Depends
+from app.api.dependencies import get_db
+from app.database.db_manager import DBManager
+from app.schemas.categories import SCategoriesAdd, SCategoriesGet
 from app.services.categories import CategoryService
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter(prefix="/categories",tags=["Category"])
 
-@router.get("/")
-async def get_categories():
- categories = await CategoryService().get_all_categories()   
+@router.get("/", response_model=list[SCategoriesGet])
+async def get_categories(session: AsyncSession = Depends(get_db)):
+
+ db_manager = DBManager(session)
+ service = CategoryService(db=db_manager)
+ categories = await service.get_all_categories()   
  return categories
 
-@router.get("/{id}")
-async def get_category(id:int):
-    category = await CategoryService().get_all_categories(id=id)   
+@router.get("/{id}", response_model=SCategoriesGet)
+async def get_category(id:int, session: AsyncSession = Depends(get_db)):
+    db_manager = DBManager(session)
+    service = CategoryService(db=db_manager)
+    category = await service.get_all_categories(id=id)   
     return category
 
-@router.post("/")
-async def add_category(category_data: SCategoriesAdd):
-    await CategoryService().add_category(category_data)
+@router.post("/", response_model=SCategoriesGet)
+async def add_category(category_data: SCategoriesAdd, session: AsyncSession = Depends(get_db)):
+    db_manager = DBManager(session)
+    service = CategoryService(db=db_manager)
+    result = await service.add_category(category_data)
+    return result 
 
 @router.put("/{id}")
-async def edit_category(id:int, category_data: SCategoriesAdd):
-    data_comment = await CategoryService().edit_category(id,category_data)
-    return data_comment
+async def edit_category(id:int, category_data: SCategoriesAdd,session: AsyncSession = Depends(get_db)):
+    db_manager = DBManager(session)
+    service = CategoryService(db=db_manager)
+    await service.edit_category(id,category_data)
+    return {"message": "Category updated successfully"}
 
 @router.delete("/{id}")
-async def delete_category(id:int):
-     await CategoryService().delete_category(id=id)   
+async def delete_category(id:int, session: AsyncSession = Depends(get_db)):
+     db_manager = DBManager(session)
+     service = CategoryService(db=db_manager)
+     await service.delete_category(id=id)   
+     return {"message": "Category deleted successfully"}
