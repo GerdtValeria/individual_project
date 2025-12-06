4f8d2b7730eee862cf13dde7453f80b1999ee6d4
dhfjvdjh <dhfjvdjh@sgdvc.com>
2025-12-06 09:37:07 +0300

    add_code

--- app/models/bookings.py
+++ app/models/bookings.py
@@ -7,13 +7,13 @@ from app.models.users import UserModel
 
 
 class BookingsModel(Base):
- __tablename__ = 'bookings'
- id = Column(Integer, primary_key=True)
- id_user = Column(Integer, ForeignKey('users.id'))
- id_rents = Column(Integer, ForeignKey('rents.id'))
- date_start = Column(Date) 
- date_end = Column(Date) 
- cost = Column(Integer) 
+    __tablename__ = 'bookings'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('users.id'))
+    id_rents = Column(Integer, ForeignKey('rents.id'))
+    date_start = Column(Date) 
+    date_end = Column(Date) 
+    cost = Column(Integer) 
 
 rent: Mapped["RentsModel"] = relationship(back_populates="booking")
 user: Mapped["UserModel"] = relationship(back_populates="bookings")
