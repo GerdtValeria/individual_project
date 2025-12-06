5513e00a1dbab58a45a64f508c5abc665075d7f4
fgsdghs <fgsdghs@hgcfg.com>
2025-12-06 19:40:57 +0300

    add_code

--- app/services/categories.py
+++ app/services/categories.py
@@ -9,21 +9,26 @@ from exceptions.categories import CategoryAlreadyExistsError, CategoryAlreadyExi
 class CategoryService(BaseService):
     
       async def get_all_categories(self) -> list[SCategoriesGet]:
-        await self.db.categories.get_all()
+        categories = await self.db.categories.get_all()
         await self.db.commit()
-        
+        return categories
+      
       async def get_category(self, category_name: int):
-        await self.db.categories.get_by_name(name=category_name)
+        category = await self.db.categories.get_by_name(name=category_name)
         await self.db.commit()
+        return category
 
       async def add_category(self, category_data: SCategoriesAdd) -> SCategoriesGet:
-        await self.db.categories.add_category(category_data)
+        category = await self.db.categories.add_category(category_data)
         await self.db.commit()
+        return category
 
       async def edit_category(self, category_id: int, category_data: SCategoriesAdd) -> None:
-        await self.db.categories.edit_category(category_id, category_data)
+        category = await self.db.categories.edit_category(category_id, category_data)
         await self.db.commit()
+        return category
 
       async def delete_category(self, id: int) -> None:
         await self.db.categories.delete_category(id)
         await self.db.commit()
+        return {"message": f"Категория с id={id} успешно удалена"} 
