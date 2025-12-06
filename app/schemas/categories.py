64cad6e43ca655683c26ff97849edd0d7b38ce70
dhfjvdjh <dhfjvdjh@sgdvc.com>
2025-12-06 10:12:55 +0300

    add_code

--- app/schemas/categories.py
+++ app/schemas/categories.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel, Field
 
 
 class SCategories(BaseModel):
+    id: int
     name: str = Field(...)
 
 class SCategoriesAdd(BaseModel):
     name: str = Field(...)
     
 class SCategoriesGet(SCategoriesAdd):
-    id: int
+    name: str = Field(...)
