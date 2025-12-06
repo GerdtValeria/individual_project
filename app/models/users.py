f7b3ece44e1ff53feb0f45783c76378b9c6b9612
dhfjvdjh <dhfjvdjh@sgdvc.com>
2025-12-06 10:03:42 +0300

    add_code

--- app/models/users.py
+++ app/models/users.py
@@ -1,10 +1,14 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.database import Base
-from app.models.bookings import BookingsModel
-from app.models.comments import CommentsModel
-from app.models.rents import RentsModel
-from app.models.roles import RoleModel
+
+
+if TYPE_CHECKING:
+    from app.models.bookings import BookingsModel
+    from app.models.comments import CommentsModel
+    from app.models.rents import RentsModel
+    from app.models.roles import RoleModel
 
 
 class UserModel(Base):
@@ -14,7 +18,7 @@ class UserModel(Base):
     role_id = Column(Integer, ForeignKey('roles.id'))
     password = Column(String(50))
 
-role: Mapped["RoleModel"] = relationship(back_populates="users")
-bookings: Mapped[list["BookingsModel"]] = relationship(back_populates="user")
-rents: Mapped[list["RentsModel"]] = relationship(back_populates="user")
-comments: Mapped[list["CommentsModel"]] = relationship(back_populates="user")
+    role: Mapped["RoleModel"] = relationship(back_populates="users")
+    bookings: Mapped[list["BookingsModel"]] = relationship(back_populates="user")
+    rents: Mapped[list["RentsModel"]] = relationship(back_populates="user")
+    comments: Mapped[list["CommentsModel"]] = relationship(back_populates="user")
