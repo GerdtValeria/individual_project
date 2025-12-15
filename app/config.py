d05c74e8d10cf06f5f9721ec6fa68cdb078433f0
GerdtValeria <gerdtvaleria33@gmail.com>
2025-12-15 20:49:24 +0300

    add_field_types

--- app/config.py
+++ app/config.py
@@ -1,21 +1,24 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    SECRET_KEY='ej08rj4wg09dnviesr03wjg'
-    ALGORITHM='HS256'
-    ACCESS_TOKEN_EXPIRE_MINUTES=60
-    DB_NAME='test.db'
+    SECRET_KEY: str = 'ej08rj4wg09dnviesr03wjg'
+    ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    DB_NAME: str = 'test.db'
+
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-    )  
+    )
 
     @property
-    def db_url(self):
-        return ('sqlite+aiosqlite:///db.sqlite3')
+    def db_url(self) -> str:
+        return 'sqlite+aiosqlite:///db.sqlite3'
 
     @property
-    def auth_data(self):
-        return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
-     
+    def auth_data(self) -> dict:
+        return {"secret_key": self.SECRET_KEY, "algorithm": self.ALGORITHM}
+
+
 settings = Settings()
