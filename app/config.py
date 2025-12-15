14edab1e8889d81efc296e8b4965b8854875d8b6
GerdtValeria <gerdtvaleria33@gmail.com>
2025-12-15 20:43:21 +0300

    add_access_token

--- app/config.py
+++ app/config.py
@@ -2,9 +2,11 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-     SECRET_KEY: str
-     ALGORITHM: str
-     model_config = SettingsConfigDict(
+    SECRET_KEY='ej08rj4wg09dnviesr03wjg'
+    ALGORITHM='HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES=60
+    DB_NAME='test.db'
+    model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     )  
 
