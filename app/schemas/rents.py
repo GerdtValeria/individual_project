d5630736037839bac2ec0c6e4aebd1f4e223f464
GerdtValeria <gerdtvaleria33@gmail.com>
2025-12-17 18:13:51 +0300

    checking

--- app/schemas/rents.py
+++ app/schemas/rents.py
@@ -42,5 +42,5 @@ class SRentGetWithRels(SRentAdd):
     model_config = ConfigDict(from_attributes=True)
 
     category: SCategoriesGet | None
-    images: list[SImagesGet] | None
+    #images: list[SImagesGet] | None
     user: SUserGet | None
