7e33f877f0d438d7bb08854443d84758cd4285f5
cdjhbcdjh <cdjhbcdj@sdu.com>
2025-12-18 17:01:24 +0300

    fix

--- app/repositories/bookings.py
+++ app/repositories/bookings.py
@@ -19,5 +19,5 @@ class BookingsRepository(BaseRepository):
         return await super().add(data)
     
     async def delete_by_id_and_user(self, booking_id: int, user_id: int) -> int:
-    return await self.delete(id=booking_id, id_user=user_id)
+        return await self.delete(id=booking_id, id_user=user_id)
 
